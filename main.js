af2883a88370148dbe0af6e8666148a7c1e4a040
aris@mail.co <aris@mail.co>
2023-09-29 17:06:15 +0900

    "new feature"

--- main.js
+++ main.js
@@ -3,38 +3,75 @@ const Todo = (function() {
 
   const formBtn = document.getElementById('form-btn');
   const formInput = document.getElementById('form-input');
-  const todos = document.getElementById('todo');
+  const todo = document.getElementById('todo');
 
   function addTask() {
     tasks.push(formInput.value)
     formInput.value = ''
-    showTasks()
+    showTask()
   }
 
-  function createEl(task) {
-    const section = document.createElement('section')
-    const span = document.createElement('span')
-    section.className = 'todo__list'
-    span.append(task)
+  function createElem(elem, name) {
+    const element = document.createElement(elem);
+    element.className = name;
+    return element;
+  }
+
+  window.addEventListener('click', function(e) {
+    if (e.target.classList.contains('todo-remove')) {
+      const section = e.target.parentElement;
+      const content = section.firstChild.textContent;
+      
+      const tasksCopy = [].concat(tasks)
+      const ind = tasksCopy.indexOf(content);
+      tasksCopy.splice(ind, 1);
+      
+      tasks = tasksCopy;
+      section.remove()
+    }
+  })
+
+  function addBtnRemove() {
+    const todoList = Array.from(
+      document.getElementsByClassName('todo-list')
+    );
+    const span = createElem(
+      'span',
+      'todo__remove todo-remove'
+    )
+    span.append('[x]')
+    
+    for (list of todoList) {
+      list.append(span)
+    }
+  }
+
+  function showTask() {
+    let ind = 0
+    for (task of tasks) {
+      ind = tasks.indexOf(task)
+    }
+    const span = createElem(
+      'span',
+      'todo__content'
+    )
+    const section = createElem(
+      'section',
+      'todo__list todo-list'
+    )
+    span.append(tasks[id])
     section.append(span)
-    return section.outerHTML
-  }
-
-  function showTasks() {
-    let content = '';
-    tasks.forEach(function(task) {
-      content += createEl(task)
-    })
-
-    todos.innerHTML = content
+    todo.append(section)
+    
+    addBtnRemove()
   }
 
   return {
-    add: function() {
+    start: function() {
       formBtn.addEventListener('click', addTask)
     }
   }
 
 })();
 
-Todo.add()
+Todo.start()
