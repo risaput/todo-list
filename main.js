888282bd085f8cb8972f92b776ca81356a8fc613
arisdevian11@gmail.com <arisdevian11@gmail.com>
2023-10-13 09:27:29 +0900

    6th commit

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ window.onload = function() {
   ];
 
   let savedTasks = JSON.parse(localStorage.getItem('todos')) || initTasks;
+  const content = document.getElementById('content');
 
   function addTask() {
     const inputText = document.getElementById('input-text');
@@ -34,28 +35,32 @@ window.onload = function() {
     return element;
   }
 
-  function handleClick(e) {
+  function handleClick(evt) {
+    const { classList } = evt.target;
     const savedTasksCopy = Array.from(savedTasks);
-    let ind = 0;
+    
+    if (classList.contains('todo-remove')) {
+      const section = evt.target.parentElement;
+      const text = section.firstChild.textContent;
 
-    if (e.target.classList.contains('todo-remove')) {
-      const section = e.target.parentElement;
-      const span = section.firstChild
-      ind = savedTasksCopy.findIndex(tsk => tsk.task == span.textContent);
-      savedTasksCopy.splice(ind, 1);
+      const idx = savedTasksCopy.findIndex(tasks => tasks.task == text);
+      savedTasksCopy.splice(idx, 1);
 
       localStorage.setItem('todos', JSON.stringify(savedTasksCopy));
       savedTasks = JSON.parse(localStorage.getItem('todos')) || []
-      e.target.parentElement.remove();
+      section.remove();
     }
 
-    if (e.target.classList.contains('todo-content')) {
-      savedTasks[ind].isDone = !savedTasks[ind].isDone;
-      const val = savedTasks[ind].isDone ? 'line-through' : 'none';
-      e.target.style.textDecoration = val;
-      console.log(ind);
-
+    if (classList.contains('todo-list')) {
+      const span = evt.target.firstChild
+      const idx = savedTasksCopy.findIndex(tasks => tasks.task == span.textContent);
+      savedTasksCopy[idx].isDone = !savedTasksCopy[idx].isDone;
+      localStorage.setItem('todos', JSON.stringify(savedTasksCopy));
+      savedTasks = JSON.parse(localStorage.getItem('todos')) || []
+      const val = savedTasksCopy[idx].isDone ? 'line-through' : 'none';
+      span.style.textDecoration = val;
     }
+
   }
 
   function addBtnRemove() {
@@ -69,11 +74,11 @@ window.onload = function() {
     span.append('❌');
 
     for (list of todoList) list.append(span);
+
   }
 
   function showTask(task) {
-    const content = document.getElementById('content');
-
+    
     const span = createElem(
       'span',
       'todo__content todo-content'
@@ -86,6 +91,7 @@ window.onload = function() {
       'section',
       'todo todo-list'
     )
+    
 
     const todo = task ? task :
       savedTasks[savedTasks.length - 1];
@@ -101,25 +107,25 @@ window.onload = function() {
     addBtnRemove()
   }
 
-  function showModal(evt) {
+  function modalToggle(evt) {
     const modal = document.getElementById('modal');
     const modalWrapper = document.getElementById('modal-wrapper');
+    const id = evt.target.id
 
-    if (evt.target.id == 'modal-btn') {
-      modal.classList.add('active');
-      modalWrapper.classList.add('active');
+    if (id == 'modal-btn') {
+      modal.classList.add('active')
+      modalWrapper.classList.add('active')
     }
-    if (evt.target.id == 'modal' || evt.target.id == 'input-add') {
+    if (id == 'modal' || id == 'input-add') {
       modal.classList.remove('active')
       modalWrapper.classList.remove('active')
     }
   }
 
-  window.addEventListener('click', showModal)
+  const inputAdd = document.getElementById('input-add')
+  inputAdd.addEventListener('click', addTask)
+  window.addEventListener('click', modalToggle)
 
-  document.getElementById('input-add')
-    .addEventListener('click', addTask);
-
-  window.addEventListener('click', handleClick);
+  content.addEventListener('click', handleClick);
 
 }
